988a3febaf64ee348797f81d8c84645efd7a49d3
Shuya Saeki <fzk03445@gmail.com>
2021-05-04 13:30:34 +0900

    Update for Vitis 2020.2

--- gen_fsbl.tcl
+++ gen_fsbl.tcl
@@ -1,4 +1,3 @@
-set hwdsgn [open_hw_design design_1_wrapper.hdf]
-generate_app -hw $hwdsgn -os standalone -proc psu_cortexa53_0 -app zynqmp_fsbl -compile -sw fsbl -dir fsbl
-quit
+set hwdsgn [hsi::open_hw_design design_1_wrapper.xsa]
+hsi::generate_app -hw $hwdsgn -os standalone -proc psu_cortexa53_0 -app zynqmp_fsbl -compile -sw fsbl -dir fsbl
 
--- gen_pmc.tcl
+++ gen_pmc.tcl
@@ -1,4 +1,5 @@
-set hwdsgn [open_hw_design design_1_wrapper.hdf]
-generate_app -hw $hwdsgn -os standalone -proc psu_pmu_0 -app zynqmp_pmufw -compile -sw pmufw -dir pmu
-quit
+setws .
+set hwdsgn [hsi::open_hw_design design_1_wrapper.xsa]
+hsi::generate_app -os standalone -hw $hwdsgn -proc psu_pmu_0 -app zynqmp_pmufw -compile -sw pmufw -dir pmu
+
 
