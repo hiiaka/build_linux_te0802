99141c9ff44d449a0bdcb81e1a4a0be2445b53ce
Shuya Saeki <fzk03445@gmail.com>
2021-05-04 13:30:55 +0900

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,24 +1,28 @@
+#!/bin/bash -eu
+
+JOBS=$[$(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g') + 1]
+
 cd hw
 rm -rf te0802_test
 vivado -mode batch -source ./create_prj.tcl
 cd ..
 
-git clone https://github.com/Xilinx/linux-xlnx.git
-git clone https://github.com/Xilinx/u-boot-xlnx.git
-git clone https://github.com/Xilinx/device-tree-xlnx.git
+git clone https://github.com/Xilinx/linux-xlnx.git -b xilinx-v2020.2 --depth 1
+git clone https://github.com/Xilinx/u-boot-xlnx.git -b xilinx-v2020.2 --depth 1
+git clone https://github.com/Xilinx/device-tree-xlnx.git -b xilinx-v2020.2 --depth 1
 git clone https://git.kernel.org/pub/scm/utils/dtc/dtc.git
-git clone https://github.com/Xilinx/arm-trusted-firmware.git
+git clone https://github.com/Xilinx/arm-trusted-firmware.git -b xilinx-v2020.2 --depth 1
 
 export CROSS_COMPILE=aarch64-linux-gnu-
 mkdir fsbl; cd fsbl
-cp ../hw/te0802_test/te0802_test.sdk/design_1_wrapper.hdf .
-hsi -source ../gen_fsbl.tcl
+cp ../hw/te0802_test/te0802_test.sdk/design_1_wrapper.xsa .
+xsct ../gen_fsbl.tcl
 cd ..
 
 export CROSS_COMPILE=aarch64-linux-gnu-
 mkdir pmc; cd pmc
-cp ../hw/te0802_test/te0802_test.sdk/design_1_wrapper.hdf .
-hsi -source ../gen_pmc.tcl
+cp ../hw/te0802_test/te0802_test.sdk/design_1_wrapper.xsa .
+xsct ../gen_pmc.tcl
 cd ..
 
 export CROSS_COMPILE=aarch64-linux-gnu-
@@ -33,15 +37,16 @@ make distclean
 patch -p1 < ../u-boot-xlnx.patch
 make xilinx_zynqmp_virt_defconfig
 export DEVICE_TREE="zynqmp-te0802-02"
-make -j8
+make -j${JOBS}
 cd ..
 
 export CROSS_COMPILE=aarch64-linux-gnu-
 cd linux-xlnx
+git checkout -b linux-xlnx-v2020.2-zynqmp-fpga refs/tags/xilinx-v2020.2
 patch -p1 < ../linux-xlnx.patch
 make ARCH=arm64 xilinx_zynqmp_defconfig
 #make ARCH=arm64 menuconfig
-make ARCH=arm64 -j8
+make ARCH=arm64 -j${JOBS}
 cd ..
 
 mkdir boot; cd boot
